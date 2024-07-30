89aab257b74af8e6362a20294b81ae9c355fa1d1
SANKET SINGH <sanketkumar0068@gmail.com>
2024-07-30 13:12:37 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 
 // MAKING CONNECTION 
-const DB = mongoose.connect('mongodb://localhost/hospital_API', { useNewUrlParser: true, useUnifiedTopology: true });
+const DB = mongoose.connect('mongodb+srv://user121:1234@cluster0.rr2mpun.mongodb.net/myMovieList?retryWrites=true&w=majority&appName=Cluster0');
 
 
 // Handle connection status
